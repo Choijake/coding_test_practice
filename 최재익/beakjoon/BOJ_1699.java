1a4a5ceb22ddff16b39653c361767610aee839d1
Choijake <shameless8@naver.com>
2025-12-29 23:56:33 +0900

    1699

---
+++ 최재익/beakjoon/BOJ_1699.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1699 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N+1];
+        dp[1] = 1;
+        for(int i=2; i<=N; i++){
+            dp[i] = i;
+
+            for(int j=1; j*j<=i; j++){
+                if(dp[i] > dp[i-j*j]+1){
+                    dp[i] = dp[i-j*j]+1;
+                }
+            }
+        }
+
+        System.out.print(dp[N]);
+    }
+}
