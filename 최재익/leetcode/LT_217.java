5b8018c3616d9dbe5aab41e5c62eacc2b31f05d8
Choijake <shameless8@naver.com>
2026-04-19 16:27:59 +0900

    Contains Duplicate

---
+++ 최재익/leetcode/LT_217.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class LT_217 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i=0; i<nums.length; i++){
+            if(set.contains(nums[i]))return true;
+            else set.add(nums[i]);
+        }
+
+        return false;
+    }
+}
